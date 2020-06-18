4cdeea18855e3eb9cf3387a349a542275d45386a
gums25011984 <gums2015@gmail.com>
2020-06-18 09:41:55 -0700

    rubah master sangsi

--- app/Http/Controllers/csangsi.php
+++ app/Http/Controllers/csangsi.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 use App\msangsi;
 class csangsi extends Controller
 {
-	  public function index()
+	  public function index(Request $request)
 		{
 			//
-			$data = \App\msangsi::all();
+			$per_page = \Request::get('per_page') ?: 100;
+			$data = \App\msangsi::paginate($per_page);
+
 		
 			if(count($data) > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
