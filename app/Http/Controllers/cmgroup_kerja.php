2478b1afa2779c62a40fa67d2697038beb8fbb87
gums25011984 <gums2015@gmail.com>
2020-06-18 09:34:43 -0700

    rubah group kerja

--- app/Http/Controllers/cmgroup_kerja.php
+++ app/Http/Controllers/cmgroup_kerja.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 use App\mmgroup_kerja;
 class cmgroup_kerja extends Controller
 {
-	  public function index()
+	 public function index(Request $request)
 		{
 			//
-			$data = \App\mmgroup_kerja::all();
+			$per_page = \Request::get('per_page') ?: 100;
+			$data = \App\mmgroup_kerja::paginate($per_page);
+			
 		
 			if(count($data) > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
