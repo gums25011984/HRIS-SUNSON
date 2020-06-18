602885a4c58be03d6fcc703cc4eb0a277e614f79
gums25011984 <gums2015@gmail.com>
2020-06-18 09:15:47 -0700

    rubah jabatan

--- app/Http/Controllers/cjabatan.php
+++ app/Http/Controllers/cjabatan.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 use App\mjabatan;
 class cjabatan extends Controller
 {
-	  public function index()
+	 public function index(Request $request)
 		{
 			//
-			$data = \App\mjabatan::all();
+			$per_page = \Request::get('per_page') ?: 100;
+			$data = \App\Mjabatan::paginate($per_page);
 		
 			if(count($data) > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
