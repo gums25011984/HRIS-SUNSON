ab5128adca6838e389351bf58bdccb420dbb602e
gums25011984 <gums2015@gmail.com>
2020-06-19 01:25:27 -0700

    pagination libur nasional

--- app/Http/Controllers/clibur_nasional.php
+++ app/Http/Controllers/clibur_nasional.php
@@ -6,10 +6,11 @@ use Illuminate\Http\Request;
 use App\mlibur_nasional;
 class clibur_nasional extends Controller
 {
-	  public function index()
+	   public function index(Request $request)
 		{
 			//
-			$data = \App\mlibur_nasional::all();
+			$per_page = \Request::get('per_page') ?: 100;
+			$data = \App\mlibur_nasional::paginate($per_page);
 		
 			if(count($data) > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
