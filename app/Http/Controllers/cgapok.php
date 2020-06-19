7c409647a35fbf91ec9ecccb0a929d4e5c3a5d42
gums25011984 <gums2015@gmail.com>
2020-06-19 01:35:01 -0700

    pagination cgapok

--- app/Http/Controllers/cgapok.php
+++ app/Http/Controllers/cgapok.php
@@ -5,25 +5,72 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\mgapok;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
 class Cgapok extends Controller
 {
-	  public function index()
+	 public function index(Request $request)
 		{
-			$data = DB::select("SELECT a.idkaryawan,b.idgapok,c.kdstatus_kerja,a.nik,a.nama, 
+			$srch = $request->srch; 
+			$per_page = $request->per_page;
+			
+			$tableIds = DB::select("SELECT a.idkaryawan,b.idgapok,c.kdstatus_kerja,a.nik,a.nama, 
 b.gaji_pokok,b.tunj_jabatan,b.tunj_prestasi,b.tunj_fungsional,b.tunj_hadir,b.tunj_rajin,
 b.tunj_masakerja,b.tunj_lainnya,
 b.pot_astek, b.pot_spsi,b.pot_koperasi,b.pot_bisnis FROM tkaryawan AS a LEFT JOIN tgapok AS b ON a.idkaryawan = b.idkaryawan
 left join tstatus_kerja as c ON a.idstatus_kerja = c.idstatus_kerja");
+$jsonResult = array();
+		
+		for($i = 0;$i < count($tableIds);$i++)
+        {
+			$jsonResult[$i]["idkaryawan"] = $tableIds[$i]->idkaryawan;
+			$jsonResult[$i]["idgapok"] = $tableIds[$i]->idgapok;
+			$jsonResult[$i]["kdstatus_kerja"] = $tableIds[$i]->kdstatus_kerja;
+			$jsonResult[$i]["nik"] = $tableIds[$i]->nik;
+			$jsonResult[$i]["nama"] = $tableIds[$i]->nama;
+			$jsonResult[$i]["gaji_pokok"] = $tableIds[$i]->gaji_pokok;
+			$jsonResult[$i]["tunj_jabatan"] = $tableIds[$i]->tunj_jabatan;
+			
+			$jsonResult[$i]["tunj_prestasi"] = $tableIds[$i]->tunj_prestasi;
+			$jsonResult[$i]["tunj_fungsional"] = $tableIds[$i]->tunj_fungsional;
+			$jsonResult[$i]["tunj_hadir"] = $tableIds[$i]->tunj_hadir;
+			$jsonResult[$i]["tunj_rajin"] = $tableIds[$i]->tunj_rajin;
+			$jsonResult[$i]["tunj_masakerja"] = $tableIds[$i]->tunj_masakerja;
+			$jsonResult[$i]["tunj_lainnya"] = $tableIds[$i]->tunj_lainnya;
+			$jsonResult[$i]["pot_astek"] = $tableIds[$i]->pot_astek;
+			
+			$jsonResult[$i]["pot_spsi"] = $tableIds[$i]->pot_spsi;
+			$jsonResult[$i]["pot_koperasi"] = $tableIds[$i]->pot_koperasi;
+			$jsonResult[$i]["pot_bisnis"] = $tableIds[$i]->pot_bisnis;
 
-			if($data > 0){ //mengecek apakah data kosong atau tidak
+			
+			
+			
+		 }
+		 if($jsonResult > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
-				$res['values'] = $data;
+				$res['values'] = $jsonResult;
+				$res = $this->paginate($jsonResult,$per_page);
 				return response($res);
 			}
 			else{
 				$res['message'] = "Empty!";
 				return response($res);
 			}
+		
+		}
+		
+		public function paginate($items,$per_page,$pageStart=1)
+		{
+			$per_page = \Request::get('per_page') ?: 100;
+			// Start displaying items from this number;
+			$offSet = ($pageStart * $per_page) - $per_page; 
+	
+			// Get only the items you need using array_slice
+			$itemsForCurrentPage = array_slice($items, $offSet, $per_page, true);
+	
+			return new LengthAwarePaginator($itemsForCurrentPage, count($items), $per_page,Paginator::resolveCurrentPage(), array('path' => Paginator::resolveCurrentPath()));
 		}
 		
 		public function store(Request $request){
