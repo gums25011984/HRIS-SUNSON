d01a907f0334e7c4f0acb5e817d79280464fc5a3
gums25011984 <gums2015@gmail.com>
2020-06-19 01:32:27 -0700

    pagination main manu

--- app/Http/Controllers/cmain_menu.php
+++ app/Http/Controllers/cmain_menu.php
@@ -6,10 +6,12 @@ use Illuminate\Http\Request;
 use App\mmain_menu;
 class cmain_menu extends Controller
 {
-	  public function index()
+	  public function index(Request $request)
 		{
 			//
-			$data = \App\mmain_menu::all();
+			$per_page = \Request::get('per_page') ?: 100;
+			$data = \App\mmain_menu::paginate($per_page);
+
 		
 			if(count($data) > 0){ //mengecek apakah data kosong atau tidak
 				$res['message'] = "Success!";
