ba1b7cef5a280a579186d8ea6f6567f953ad3526
gums2015 <gums2015@gmail.com>
2020-06-26 10:27:06 -0700

    role permission terbaru

--- app/Http/Controllers/crole_permission.php
+++ app/Http/Controllers/crole_permission.php
@@ -18,25 +18,21 @@ class crole_permission extends Controller
 		$idjabatan = $request->idjabatan; 
 		$iddivisi = $request->iddivisi; 
 		$iddepartemen = $request->iddepartemen; 
-		$sort = \Request::get('sort') ?: 'idaccess';
+		$sort = \Request::get('sort') ?: 'idpermission';
 	/*	$tableIds = DB::select( DB::raw("SELECT idjabatan,jabatan,'' as karyawan FROM tjabatan"));*/
-		$tableIds = DB::select( DB::raw("SELECT  b.idaccess,a.idpermission,a.idjabatan,d.idmenuitem,f.departemen,g.divisi,a.idjabatan,a.iddivisi,a.iddepartemen,
-	d.nameof ,d.filename ,a.read,a.create,a.update,a.delete
-	FROM permission_role AS a 
+		$tableIds = DB::select( DB::raw("SELECT a.idpermission,b.`nameof`,b.`urlpermission`, a.create,a.read,a.update,a.delete,
+a.idjabatan,a.iddivisi,a.iddepartemen
+FROM permission_role AS a LEFT JOIN permission AS b ON a.idpermission = b.`idpermission`
 	
-	  LEFT JOIN sysuseraccess AS b ON a.idjabatan = b.`idjabatan`
-	LEFT JOIN sysappmenuitem AS d ON b.`fcode` = d.`code`  LEFT JOIN sysappmenu AS e ON e.`code` = d.`fcode`
-	LEFT JOIN tdepartemen AS f ON a.`iddepartemen` = f.`iddepartemen` LEFT JOIN tdivisi AS g ON a.`iddivisi` = g.`iddivisi`
-	
-WHERE idmenuitem IS NOT NULL and (a.idjabatan =  '$idjabatan' and a.iddepartemen =  '$iddepartemen' and a.iddivisi =  '$iddivisi')  "));
+WHERE  (a.idjabatan =  '$idjabatan' and a.iddepartemen =  '$iddepartemen' and a.iddivisi =  '$iddivisi')  "));
 				
         $jsonResult = array();
 
         for($i = 0;$i < count($tableIds);$i++)
         {
-		    $jsonResult[$i]["id_permission"] = $tableIds[$i]->idaccess;
+		    $jsonResult[$i]["id_permission"] = $tableIds[$i]->idpermission;
 			$jsonResult[$i]["name_permission"] = $tableIds[$i]->nameof;
-			$jsonResult[$i]["url_permission"] = $tableIds[$i]->filename;
+			$jsonResult[$i]["url_permission"] = $tableIds[$i]->urlpermission;
 			$jsonResult[$i]["create"] = $tableIds[$i]->create;
 			$jsonResult[$i]["read"] = $tableIds[$i]->read;
 			$jsonResult[$i]["update"] = $tableIds[$i]->update;
